fb05c3a915300ec6cbaa9c035ce2f16a36d5a299
ismael <isMontelongo@outlook.com>
2024-01-16 17:26:45 +0000

    base

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\User;
+use App\Models\post;
+
+class PostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'user_id' => User::factory(),
+        ];
+    }
+}
